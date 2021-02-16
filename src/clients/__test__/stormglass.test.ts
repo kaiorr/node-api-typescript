2992a13f2bafbb889211b10d03f3b44fcc7fe6a9
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-02-15 23:13:29 -0300

    feat :: update :: force inference types, only use tests cases jest.mocked<typeof axios>

--- src/clients/__test__/stormglass.test.ts
+++ src/clients/__test__/stormglass.test.ts
@@ -6,13 +6,16 @@ import stormGlassNormalizedFixture from '@test/fixtures/stormGlass_normalized_re
 jest.mock('axios')
 
 describe('StormGlass client', () => {
+  //force inference types, only use tests cases || jest.mocked<>
+  const mockedAxios = axios as jest.Mocked<typeof axios>
+
   it('Should return the normalized forecast from the StormGlass service', async () => {
     const lat = -16.3714122
     const lng = -49.5162334
+    //force inference types, only use tests cases || jest.mocked<>
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeatherFixture })
 
-    axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeatherFixture })
-
-    const stormGlass = new StormGlass(axios)
+    const stormGlass = new StormGlass(mockedAxios)
     const response = await stormGlass.fetchPoints(lat, lng)
     expect(response).toEqual(stormGlassNormalizedFixture)
   })
